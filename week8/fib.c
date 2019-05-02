b05f0d1c66fd8164ffabbd2df74e77c6d5896436
Brittany Evat <brittany@perxapp.com.au>
2019-05-02 19:23:54 +1000

    adding week 8 and 9 files

---
+++ week8/fib.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int fibR(int n, int *knownFib) {
+    // TODO
+}
+
+int fib(int n) {
+    assert(n > 0);
+    int *knownFib = calloc(n+1, sizeof(int));
+    assert(knownFib != NULL);
+
+    // TODO
+}
